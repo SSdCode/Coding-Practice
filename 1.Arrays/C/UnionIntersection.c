3d1fb39b9e0671f0598c21f4885f68751ef9c8bd
SD3146 <samirdubey.contact@gmail.com>
2025-04-27 22:10:09 +0530

    UnionIntersection in C

---
+++ 1.Arrays/C/UnionIntersection.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int unionCount = 0;
+int intersectionCount = 0;
+
+void insertInArray(int element, int *array, char InorUni)
+{
+    bool isElementPresentInArray = false;
+    for (int k = 0; k < 6; k++)
+    {
+        if (array[k] == element)
+        {
+            isElementPresentInArray = true;
+        }
+    }
+    if (!isElementPresentInArray)
+    {
+        if (InorUni == 'u')
+        {
+            array[unionCount++] = element;
+        }
+        else
+        {
+            array[intersectionCount++] = element;
+        }
+    }
+}
+
+int main()
+{
+    int arr1[] = {1, 3, 2, 4, 6, 7};
+    int arr2[] = {4, 6, 7, 8, 9, 5};
+
+    int elementArr1 = arr1[0];
+    int elementArr2 = arr2[0];
+
+    int unionn[6];
+    int intercection[12];
+
+    for (int i = 0; i < 6; i++)
+    {
+        elementArr1 = arr1[i];
+        elementArr2 = arr2[i];
+        bool isElementArr1PresentInArr2 = false;
+        bool isElementArr2PresentInArr1 = false;
+        for (int j = 0; j < 6; j++)
+        {
+            if (arr1[j] == elementArr2)
+            {
+                isElementArr2PresentInArr1 = true;
+            }
+            if (arr2[j] == elementArr1)
+            {
+                isElementArr1PresentInArr2 = true;
+            }
+        }
+        if (isElementArr2PresentInArr1)
+        {
+            insertInArray(elementArr2, intercection, 'i');
+        }
+        if (isElementArr1PresentInArr2)
+        {
+            insertInArray(elementArr1, intercection, 'i');
+        }
+        insertInArray(elementArr2, unionn, 'u');
+        insertInArray(elementArr1, unionn, 'u');
+    }
+
+    printf("intersection - ");
+
+    for (int m = 0; m < intersectionCount; m++)
+    {
+        printf("%d, ", intercection[m]);
+    }
+    printf("\n");
+    printf("Union - ");
+    for (int n = 0; n < unionCount; n++)
+    {
+        printf("%d, ", unionn[n]);
+    }
+
+    return 0;
+}
